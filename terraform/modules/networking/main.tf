92e98895a57bb531e24ecaa8d391223d9cf06ade
Gre4kas <misaa3818@gmail.com>
2025-02-02 17:00:12 +0200

    Remove duble code

--- terraform/modules/networking/main.tf
+++ terraform/modules/networking/main.tf
@@ -62,25 +62,3 @@ resource "aws_vpc_security_group_egress_rule" "all_egress_eb" {
   to_port           = 0
   description       = "Allow all outbound traffic"
 }
-
-
-# Output the VPC and Security Group details
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = module.vpc.vpc_id
-}
-
-output "public_subnet_ids" {
-  description = "List of public subnet IDs"
-  value       = module.vpc.public_subnets
-}
-
-output "private_subnet_ids" {
-  description = "List of private subnet IDs"
-  value       = module.vpc.private_subnets
-}
-
-output "elastic_beanstalk_sg_id" {
-  description = "ID of the Security Group for Elastic Beanstalk instances"
-  value       = aws_security_group.elastic_beanstalk_sg.id
-}
